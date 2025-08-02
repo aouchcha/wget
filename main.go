c3bc850e501173bdebadb60a26efacea4be716bf
aouchcha <ouchchatea@gmail.com>
2025-08-02 13:21:47 +0100

    parsing done enchaelaaah

--- main.go
+++ main.go
@@ -13,9 +13,10 @@ type FlagsComponents struct {
 	OutputFile string
 	PathFile   string
 	RateLimite int
-	Exclude    string
-	Reject     string
-	Mirror     bool
+	Exclude    []string
+	Reject     []string
+	isMirror   bool
+	// Mirror     string
 	Background bool
 	Convert    bool
 }
@@ -27,13 +28,14 @@ func main() {
 		return
 	}
 	components := FlagsComponents{}
+	var flags = []string{"-O", "-B", "-P", "--rate-limit", "--mirror", "-R", "--reject", "-X", "--exclude"}
 	// fmt.Println(args, len(args))
 	i := 0
 	for i < len(args) {
 		if strings.HasPrefix(args[i], "http") || strings.HasPrefix(args[i], "ftp") {
 			components.Link = args[i]
 		} else if strings.HasPrefix(args[i], "-O") && i <= len(args)-2 {
-				checker, err := CatchOutputFile(args[i:i+2], &components)
+			checker, err := CatchOutputFile(args[i:i+2], &components, flags)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				return
@@ -43,7 +45,7 @@ func main() {
 				continue
 			}
 		} else if strings.HasPrefix(args[i], "-P") && i <= len(args)-2 {
-			checker, err := CatchPath(args[i:i+2], &components)
+			checker, err := CatchPath(args[i:i+2], &components, flags)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				return
@@ -52,8 +54,34 @@ func main() {
 				i += 2
 				continue
 			}
-		}else if strings.HasPrefix(args[i], "--") && i <= len(args)-2 {
-			checker, err := CatchRate(args[i:i+2], &components)
+		} else if strings.HasPrefix(args[i], "--rate-limit") && i <= len(args)-2 {
+			checker, err := CatchRate(args[i:i+2], &components, flags)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			if checker {
+				i += 2
+				continue
+			}
+		} else if strings.HasPrefix(args[i], "--mirror") {
+			if !CheckValidFlag(args[i], flags) {
+				fmt.Fprintln(os.Stderr, "invalid flag --mirror")
+				return
+			}
+			components.isMirror = true
+		} else if strings.HasPrefix(args[i], "-R") || strings.HasPrefix(args[i], "--reject") && i <= len(args)-2 {
+			checker, err := CatchTheRejectedSuffix(args[i:i+2], &components, flags)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			if checker {
+				i += 2
+				continue
+			}
+		} else if strings.HasPrefix(args[i], "-X") || strings.HasPrefix(args[i], "--exclude") && i <= len(args)-2 {
+			checker, err := CatchTheRExcludedFolders(args[i:i+2], &components, flags)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				return
@@ -72,16 +100,31 @@ func main() {
 	fmt.Println(components)
 }
 
-func CatchOutputFile(args []string, comp *FlagsComponents) (bool, error) {
+func CheckValidFlag(f string, flags []string) bool {
+	for _,flag := range flags {
+		if flag == f {
+			return  true
+		}
+	}
+	return false
+}
+
+func CatchOutputFile(args []string, comp *FlagsComponents, flags []string) (bool, error) {
 	if strings.Contains(args[0], "=") {
 		sli := strings.Split(args[0], "=")
+		if !CheckValidFlag(sli[0], flags) {
+			return false, errors.New("invalid flag -O")
+		}
 		if len(sli) != 2 || sli[1] == "" {
 			return false, errors.New("missing output file while presence of the -O flag")
-		} else {
-			comp.OutputFile = sli[1]
-			return false, nil
-		}
+		} 	
+		comp.OutputFile = sli[1]
+		return false, nil
+		
 	} else {
+		if !CheckValidFlag(args[0], flags) {
+			return false, errors.New("invalid flag -O")
+		}
 		if args[1] == "" || strings.HasPrefix(args[1], "http") {
 			return false, errors.New("missing output file while presence of the -O flag")
 		}
@@ -90,16 +133,22 @@ func CatchOutputFile(args []string, comp *FlagsComponents) (bool, error) {
 	}
 }
 
-func CatchPath(args []string, comp *FlagsComponents) (bool, error) {
+func CatchPath(args []string, comp *FlagsComponents, flags []string) (bool, error) {
 	if strings.Contains(args[0], "=") {
 		sli := strings.Split(args[0], "=")
+		if !CheckValidFlag(sli[0], flags) {
+			return false, errors.New("invalid flag -P")
+		}
 		if len(sli) != 2 || sli[1] == "" {
 			return false, errors.New("missing path while presence of the -P flag")
-		} else {
-			comp.PathFile = sli[1]
-			return false, nil
-		}
+		} 
+		comp.PathFile = sli[1]
+		return false, nil
+		
 	} else {
+		if !CheckValidFlag(args[0], flags) {
+			return false, errors.New("invalid flag -P")
+		}
 		if args[1] == "" || strings.HasPrefix(args[1], "http") {
 			return false, errors.New("missing path while presence of the -P flag")
 		}
@@ -108,35 +157,103 @@ func CatchPath(args []string, comp *FlagsComponents) (bool, error) {
 	}
 }
 
-func CatchRate(args []string, comp *FlagsComponents) (bool, error) {
+func CatchRate(args []string, comp *FlagsComponents, flags []string) (bool, error) {
 	// fmt.Println(args)
 	if strings.Contains(args[0], "=") {
 		sli := strings.Split(args[0], "=")
+		if !CheckValidFlag(sli[0], flags) {
+			return false, errors.New("invalid flag --rate-limit")
+		}
 		// fmt.Println(sli)
 		if len(sli) != 2 || sli[1] == "" {
 			return false, errors.New("missing the rate while presence of the --rate-limit flag")
-		} else {
-			rate := sli[1]
-			
-			holder, err := strconv.Atoi(rate[:len(rate)-1])
-			// fmt.Println(holder)
-			if err!=nil {
-				return false, errors.New("the rate isn't a valid number")
-			}
-			comp.RateLimite = holder
-			return false, nil
+		} 
+		rate := sli[1]
+
+		holder, err := strconv.Atoi(rate[:len(rate)-1])
+		// fmt.Println(holder)
+		if err != nil {
+			return false, errors.New("the rate isn't a valid number")
 		}
+		comp.RateLimite = holder
+		return false, nil
+		
 	} else {
+		if !CheckValidFlag(args[0], flags) {
+			return false, errors.New("invalid flag --rate-limit")
+		}
 		if args[1] == "" {
 			return false, errors.New("missing the rate while presence of the --rate-limit flag")
 		}
 		rate := args[1]
 		holder, err := strconv.Atoi(rate[:len(rate)-1])
 		// fmt.Println(holder)
-		if err!=nil {
+		if err != nil {
 			return false, errors.New("the rate isn't a valid number")
 		}
 		comp.RateLimite = holder
 		return true, nil
 	}
+}
+
+func CatchTheRejectedSuffix(args []string, comp *FlagsComponents, flags []string) (bool, error) {
+	if !comp.isMirror {
+		return false, errors.New("flag --mirror is missing")
+	}
+	var rejectEx string
+	var checker bool
+	if strings.Contains(args[0], "=") {
+		sli := strings.Split(args[0], "=")
+		if !CheckValidFlag(sli[0], flags) {
+			return false, errors.New("invalid flag -R || --reject")
+		}
+		if len(sli) != 2 || sli[1] == "" {
+			return false, errors.New("missing rejected extentions while presence of the -R || --reject flag")
+		}
+		rejectEx = sli[1]
+		checker = false
+		
+	} else {
+		if !CheckValidFlag(args[0], flags) {
+			return false, errors.New("invalid flag -R || --reject")
+		}
+		if args[1] == "" || strings.HasPrefix(args[1], "http") {
+			return false, errors.New("missing rejected extentions while presence of the -R || --reject flag")
+		}
+		rejectEx = args[1]
+		checker = true
+	}
+	comp.Reject = strings.Split(rejectEx, ",")
+	return checker, nil
+}
+
+func CatchTheRExcludedFolders(args []string, comp *FlagsComponents, flags []string) (bool, error) {
+	if !comp.isMirror {
+		return false, errors.New("flag --mirror is missing")
+	}
+	var ExcludFolfers string
+	var checker bool
+	if strings.Contains(args[0], "=") {
+		sli := strings.Split(args[0], "=")
+		if !CheckValidFlag(sli[0], flags) {
+			return false, errors.New("invalid flag -X || --exclud")
+		}
+		if len(sli) != 2 || sli[1] == "" {
+			return false, errors.New("missing rejected extentions while presence of the -R || --reject flag")
+		} else {
+			ExcludFolfers = sli[1]
+			checker = false
+		}
+	} else {
+		if !CheckValidFlag(args[0], flags) {
+			return false, errors.New("invalid flag -X || --exclud")
+		}
+		if args[1] == "" || strings.HasPrefix(args[1], "http") {
+			return false, errors.New("missing rejected extentions while presence of the -R || --reject flag")
+		}
+		ExcludFolfers = args[1]
+		checker = true
+	}
+	comp.Exclude = strings.Split(ExcludFolfers, ",")
+	return checker, nil
 }
